045bd41ff92419373c1297de7dc8856333e465da
Adrian <adrian.e.rosales@gmail.com>
2019-08-23 23:51:58 -0700

    trying

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 'use strict';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app-test';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || ' https://afternoon-badlands-93771.herokuapp.com';
 exports.PORT = process.env.PORT || 8080;  
