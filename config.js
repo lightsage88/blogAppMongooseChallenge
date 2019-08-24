405cc7656f87cc24c22f524319bdbce5d4c749dc
Adrian <adrian.e.rosales@gmail.com>
2019-08-24 00:19:37 -0700

    charming notion

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app-test';
-exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://lightsage88:Walruses8@my-first-atlas-db-obkbt.mongodb.net/blogAppMongooseChallenge?retryWrites=true&w=majority';
 exports.PORT = process.env.PORT || 8080;  
